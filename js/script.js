7a549429fb75f02b75bce88f40d27bdab0a98153
KBOlifant <241299@virtualwindow.co.za>
2024-06-11 15:28:38 +0200

    updates fellas

--- js/script.js
+++ js/script.js
@@ -1,16 +1,58 @@
 
+// Get the modal
+var modal = document.getElementById("myModalContainer");
+
+// Get the button that opens the modal
+var btn = document.getElementById("basketLogo");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
 //setting up increasing and decreasing functions to add tickets in the flights age
-function incrementResults(){
-    if(document.getElementById('result1').value < 10){ //can only support up to 9 tickets
-        document.getElementById('result1').value += 1;
+function incrementResults(result){
+    let val = parseInt(document.getElementById(result).value);
+    if(val < 99){ //can only support up to 9 tickets
+        val ++;
     }
-    console.log(document.getElementById('result1').value); //making sure i have the correct values
+    document.getElementById(result).value = val;
+    
+    console.log(val); //making sure i have the correct values
 }
 
-function decrementResults(){
-    if(document.getElementById('result1').value > 0){ //prevent the system from ordering negative tickets
-        document.getElementById('result1').value -= 1;
+function decrementResults(result){
+    let val = parseInt(document.getElementById(result).value);
+    if(val > 0){ //prevent the system from ordering negative tickets
+        
+        val -= 1;
     }
-    
-    console.log(document.getElementById('result1').value); //making sure i have the correct values again
+    document.getElementById(result).value = val;
+    console.log(val); //making sure i have the correct values again
+}
+
+function bookNow(result, Item){
+    let val = parseInt(document.getElementById(result).value);
+    let itemsInCart = parseInt(document.getElementById('itemsInCart').innerHTML) + val;
+    document.getElementById('itemsInCart').innerHTML = itemsInCart;
+    document.getElementById('c_'+result).value = val; //updating cart values
+
+    if(val > 0){
+        document.getElementById(Item).style.display = "block";
+    }
+} 
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
