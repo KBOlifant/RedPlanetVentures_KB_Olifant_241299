c24d16720b44a06614dc2088ff85c71a59266e88
KBOlifant <241299@virtualwindow.co.za>
2024-06-12 13:56:29 +0200

    uppdates fellas

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 
-// Get the modal
-var modal = document.getElementById("myModalContainer");
+// Get the modal Container
+var modalContainer = document.getElementById("myModalContainer");
+
+//Get the modal
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("basketLogo");
@@ -15,8 +18,6 @@ function incrementResults(result){
         val ++;
     }
     document.getElementById(result).value = val;
-    
-    console.log(val); //making sure i have the correct values
 }
 
 function decrementResults(result){
@@ -26,14 +27,53 @@ function decrementResults(result){
         val -= 1;
     }
     document.getElementById(result).value = val;
-    console.log(val); //making sure i have the correct values again
+}
+
+function incrementTickets(txt_amt, TicketAmt){
+    let val = parseInt(document.getElementById(TicketAmt).value);
+    if(val < 99){ //can only support up to 9 tickets
+        val ++;
+        document.getElementById(TicketAmt).value = val;
+        let itemsInCart = parseInt(document.getElementById('itemsInCart').innerHTML) + 1;
+        document.getElementById('itemsInCart').innerHTML = itemsInCart;
+    }
+    updateTicketAmt(txt_amt, val);
+}
+
+function decrementTickets(txt_amt, TicketAmt){
+    let val = parseInt(document.getElementById(TicketAmt).value);
+    let removeTicket = txt_amt.substr(2, txt_amt.length)
+    if(val > 1){ //can only support up to 9 tickets
+        val --;
+        document.getElementById(TicketAmt).value = val;
+        let itemsInCart = parseInt(document.getElementById('itemsInCart').innerHTML) - 1;
+        document.getElementById('itemsInCart').innerHTML = itemsInCart;
+    } else{
+        RemoveTicket(TicketAmt, removeTicket)
+    }
+    updateTicketAmt(txt_amt, val);
+}
+
+function RemoveTicket(resultAmt, Ticket){
+    let val = parseInt(document.getElementById(resultAmt).value);
+    let itemsInCart = parseInt(document.getElementById('itemsInCart').innerHTML) - val;
+    document.getElementById('itemsInCart').innerHTML = itemsInCart;
+    document.getElementById(Ticket).style.display = "none";
+    document.getElementById(resultAmt).value = 0;
+}
+
+function updateTicketAmt(txt_amt, amount){
+    document.getElementById(txt_amt).innerText = "You Currently Have "+amount+" Tickets";
 }
 
 function bookNow(result, Item){
     let val = parseInt(document.getElementById(result).value);
     let itemsInCart = parseInt(document.getElementById('itemsInCart').innerHTML) + val;
+    let cartAmt = parseInt(document.getElementById('c_'+result).value);
+    cartAmt += val;
     document.getElementById('itemsInCart').innerHTML = itemsInCart;
-    document.getElementById('c_'+result).value = val; //updating cart values
+    document.getElementById('c_'+result).value = cartAmt;
+    updateTicketAmt('c_'+Item, cartAmt);
 
     if(val > 0){
         document.getElementById(Item).style.display = "block";
@@ -42,17 +82,17 @@ function bookNow(result, Item){
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  modalContainer.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modalContainer.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modalContainer.style.display = "none";
   }
 }
